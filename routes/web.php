acefefbecd0aa2d1037cd5e4dac75f595fde565d
Hồ Diên Lợi <103028355+hodienloi1981@users.noreply.github.com>
2023-03-28 08:11:25 +0700

    Update web.php
    Làm nhanh lên em

--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,4 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\frontend\SiteController;
 
 Route::get('/',[SiteController::class,'index'])->name('site.home');
+#THẦY LỢI, KHÔNG CÓ CÁI GÌ LÀ SAO????????????????
